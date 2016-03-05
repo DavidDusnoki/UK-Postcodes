3a991fe98a219d6f5e5b2931efb0c61fddea2a4b
David Dusnoki <david.dusnoki@nativ.tv>
2016-03-05 12:12:55 +0000

    Add countries table

---
+++ mysql/data_counties.sql
@@ -0,0 +1,193 @@
+-- phpMyAdmin SQL Dump
+-- version 4.5.2
+-- http://www.phpmyadmin.net
+--
+-- Host: 127.0.0.1
+-- Generation Time: Mar 04, 2016 at 08:44 AM
+-- Server version: 5.7.9
+-- PHP Version: 5.6.16
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `test`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `data_counties`
+--
+
+--
+-- Dumping data for table `data_counties`
+--
+
+INSERT INTO `data_counties` (`county`) VALUES
+("Aberdeen City"),
+("Aberdeenshire"),
+(""),
+("Angus"),
+("Moray"),
+("Highland"),
+("Hertfordshire"),
+("West Midlands"),
+("Hereford and Worcester"),
+("Warwickshire"),
+("Staffordshire"),
+("Bath Avon"),
+("Somerset"),
+("Wiltshire"),
+("Bristol Avon"),
+("Dorset"),
+("Lancashire"),
+("West Yorkshire"),
+("Yorkshire, West"),
+("Bradford"),
+("North Yorkshire"),
+("Bournemouth"),
+("Poole"),
+("Hampshire"),
+("Greater Manchester"),
+("Brighton and Hove"),
+("East Sussex"),
+("West Sussex"),
+("Sussex"),
+("Greater London"),
+("Kent"),
+("Bristol"),
+("South Gloucestershire"),
+("North Somerset"),
+("Belfast"),
+("County Down"),
+("County Londonderry"),
+("County Armagh"),
+("County Tyrone"),
+("County Fermanagh"),
+("Cumbria"),
+("Northumberland"),
+("Cambridgeshire"),
+("Essex"),
+("Suffolk"),
+("Cardiff"),
+("Vale of Glamorgan"),
+("Caerphilly"),
+("Newport"),
+("Bridgend"),
+("Rhondda Cynon Taff"),
+("Powys"),
+("Merthyr Tydfil"),
+("Glamorgan"),
+("Cheshire"),
+("Flintshire"),
+("Merseyside"),
+("Denbighshire"),
+("Surrey"),
+("Leicestershire"),
+("Northamptonshire"),
+("Shropshire"),
+("Dundee City"),
+("Perth and Kinross"),
+("Fife"),
+("Derby"),
+("Derbyshire"),
+("Dumfries and Galloway"),
+("East Ayrshire"),
+("County Durham"),
+("Tyne and Wear"),
+("Darlington"),
+("South Yorkshire"),
+("Nottinghamshire"),
+("E Riding of Yorkshire"),
+("North Lincolnshire"),
+("Lincolnshire"),
+("Yorkshire, South"),
+("North Eart Lincolnshire"),
+("Lincolnshire (South Humberside)"),
+("Devon"),
+("City of Edinburgh"),
+("Midlothian"),
+("East Lothian"),
+("Scottish Borders"),
+("West Lothian"),
+("South Lanarkshire"),
+("Falkirk"),
+("Cornwall"),
+("Clackmannanshire"),
+("Stirling"),
+("Argyll and Bute"),
+("Glasgow City"),
+("Dunbartonshire"),
+("North Lanarkshire"),
+("East Renfrewshire"),
+("West Dunbart"),
+("East Dunbartonshire"),
+("Gloucestershire"),
+("Oxfordshire"),
+("Berkshire"),
+("Guernsey Channel Islands"),
+("Channel Islands"),
+("Yorkshire, North"),
+("Buckinghamshire"),
+("Western Isles"),
+("Kingston upon Hull"),
+("Yorkshire, East (North Humberside)"),
+("Isle of Man"),
+("Norfolk"),
+("Banffshire"),
+("Morayshire"),
+("Jersey Channel Islands"),
+("South Ayrshire"),
+("North Ayrshire"),
+("Orkney Islands"),
+("Orkney"),
+("Leicester"),
+("Rutland"),
+("Wrexham"),
+("Conwy"),
+("Gwynedd"),
+("Isle of Anglesey"),
+("Leeds"),
+("Luton"),
+("Bedfordshire"),
+("Milton Keynes"),
+("Tyne & Wear"),
+("Blaenau Gwent"),
+("Monmouthshire"),
+("Torfaen"),
+("Renfrewshire"),
+("Inverclyde"),
+("Isle of Barra"),
+("Isle of South Uist"),
+("Isle of North Uist"),
+("Portsmouth"),
+("Isle of Wight"),
+("Swansea"),
+("Neath Port Talbot"),
+("Carmarthenshire"),
+("Pembrokeshire"),
+("Ceredigion"),
+("Swindon"),
+("Southampton"),
+("Stoke-on-Trent"),
+("Isles of Scilly"),
+("Middlesbrough"),
+("Redcar and Cleveland"),
+("Stockton-on-Tees"),
+("Hartlepool"),
+("Cleveland"),
+("Middlesex"),
+("Wolverhampton"),
+("York"),
+("Shetland Islands");
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
